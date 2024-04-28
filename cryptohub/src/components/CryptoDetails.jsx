e3409bd5d6b4679675819c18c0e8944dcd07e2ea
Tanay <tanayjhunjhunwala.official@gmail.com>
2024-04-28 17:40:08 +0530

    added favicon and remove bug from 24hVloume

--- cryptohub/src/components/CryptoDetails.jsx
+++ cryptohub/src/components/CryptoDetails.jsx
@@ -20,13 +20,12 @@ const CryptoDetails = () => {
   const {data : coinHistory} = useGetCryptoHistoryQuery({coinId , timeperiod});
   const cryptoDetails = data?.data?.coin;
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
-
   if(isFetching) return <Loader />;
   // console.log(cryptoDetails);
   const stats = [
     { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
     { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-    { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
+    { title: '24h Volume', value: `$ ${cryptoDetails?.['24hVolume'] && millify(cryptoDetails?.['24hVolume'])}`, icon: <ThunderboltOutlined /> },
     { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
     { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
   ];
